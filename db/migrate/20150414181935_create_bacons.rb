70e8311b12a21ad5895e80891b23514e6feafcec
Felix Krause <KrauseFx@gmail.com>
2015-04-14 20:34:39 +0100

    Added bacons and bacon migration

---
+++ db/migrate/20150414181935_create_bacons.rb
@@ -0,0 +1,39 @@
+class CreateBacons < ActiveRecord::Migration
+  def change
+    create_table :bacons do |t|
+      t.string :tool
+      t.date :launch_date
+      t.integer :launches
+
+      t.timestamps null: false
+    end
+
+    # Migrate existing data
+    start_time = Time.at(1427068800) # the first day
+    step = 1.day
+
+    UpdateCheck.order(:count).reverse.each do |check|
+      current_time = start_time
+      while current_time <= Time.now
+        current = {
+          tool: check.tool,
+          launches: 0,
+          launch_date: start_time.to_date
+        }
+
+        check["data"].each do |t|
+          if Time.at(t) > current_time and Time.at(t) < (current_time + step)
+            current[:launches] += 1
+          end
+        end
+
+        if current[:launches] > 0
+          Bacon.create!(current)
+          puts current
+        end
+
+        current_time += step
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150322131142) do
+ActiveRecord::Schema.define(version: 20150414181935) do
+
+  create_table "bacons", force: :cascade do |t|
+    t.string   "tool"
+    t.date     "launch_date"
+    t.integer  "launches"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "update_checks", force: :cascade do |t|
     t.string   "tool"
